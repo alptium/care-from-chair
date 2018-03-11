d7ed1d467f29ecd48829b59a1d76b9bceaf761a8
vasilijajoksimovic <35069869+vasilijajoksimovic@users.noreply.github.com>
2018-03-11 11:13:03 +0100

    Uploaded

--- alptium-healthcare/src/com/alptium/healthcare/Patient.java
+++ alptium-healthcare/src/com/alptium/healthcare/Patient.java
@@ -9,14 +9,14 @@ public class Patient {
 	private String dateOfBirth;
 	private String placeOfBirth;
 	private String gender;
-	private String ID;
+	private String id;
 	private String address;
 	private String town;
 	private String citizenship;
 	private String phoneNumber;
 
 	
-	public Patient(String name, String parentName, String surname, String jmbg, String dateOfBirth, String placeOfBirth, String gender, String ID, String address, String town, String citizenship, String phoneNumber ) {
+	public Patient(String name, String parentName, String surname, String jmbg, String dateOfBirth, String placeOfBirth, String gender, String id, String address, String town, String citizenship, String phoneNumber ) {
 		this.name = name;
 		this.parentName = parentName;
 		this.surname = surname;
@@ -24,7 +24,7 @@ public class Patient {
 		this.dateOfBirth = dateOfBirth;
 		this.placeOfBirth = placeOfBirth;
 		this.gender = gender;
-		this.ID = ID;
+		this.id = id;
 		this.address = address;
 		this.town = town;
 		this.citizenship = citizenship;
@@ -32,12 +32,12 @@ public class Patient {
 		
 	}
 	
-	public Patient(String name, String parentName, String surname, String jmbg, String ID) {
+	public Patient(String name, String parentName, String surname, String jmbg, String id) {
 		this.name = name;
 		this.parentName = parentName;
 		this.surname = surname;
 		this.jmbg = jmbg;
-		this.ID = ID;
+		this.id = id;
 	}
 	
 	protected void checkJmbg(String jmbg) {
@@ -47,6 +47,7 @@ public class Patient {
 			}
 	}
 	public void setJmbg(String jmbg) {
+		
 		try{
 		checkJmbg(jmbg);
 		this.jmbg = jmbg;
@@ -55,45 +56,64 @@ public class Patient {
 		System.out.println("JMBG must be 13 character long");
 		System.exit(1);
 		}
-		}
-		public void setName(String name) {
+		
+	}
+	
+	public String getJmbg() {
+		return jmbg;
+	}
+	
+	public void setName(String name) {
 		this.name = name;
-		}
-		public String getName() {
+	}
+	
+	public String getName() {
 		return name;
-		}
-		public String getParentName() {
-			return parentName;
-			}
-		public void setSurname(String surname) {
+	}
+	
+	public String getParentName() {
+		return parentName;
+	}
+	
+	public void setSurname(String surname) {
 		this.surname = surname;
-		}
-		public String getSurname() {
+	}
+	
+	public String getSurname() {
 		return surname;
-		}
-		public String getDateOfBirth() {
-			return dateOfBirth;
-		}
-		public String getPlaceOfBirth() {
-			return placeOfBirth;
-		}
-		public String getGender() {
-			return gender;
-		}
-		public String getID() {
-			return ID;
-		}
-		public String getAddress() {
-			return address;
-		}
-		public String getTown() {
-			return town;
-		}
-		public String getCitizenship() {
-			return citizenship;
-		}
-		public String getPhoneNumber() {
-			return phoneNumber;
-		}
+	}
+	
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	
+	public String getPlaceOfBirth() {
+		return placeOfBirth;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public String getTown() {
+		return town;
+	}
+	
+	public String getCitizenship() {
+		return citizenship;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
 }
 
